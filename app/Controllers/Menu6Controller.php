baed12df67fe173f005bd16e2284b75eae9f6ada
KIMKYEONGYONG <rlaruddyd1@naver.com>
2024-10-05 19:53:00 +0900

    운정 front

--- app/Controllers/Menu6Controller.php
+++ app/Controllers/Menu6Controller.php
@@ -6,7 +6,10 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Enum\Board\BoardType;
 use App\Interfaces\AuthInterface;
+use App\Services\BoardService;
+use Doctrine\ORM\Exception\NotSupported;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
@@ -15,12 +18,18 @@ class Menu6Controller extends Controller
 {
     public function __construct(
         private readonly Twig $twig,
+        private readonly BoardService $boardService,
     ) {
     }
 
+    /**
+     * @throws NotSupported
+     */
     public function notice(Request $request, Response $response): Response
     {
-        return $this->render($this->twig,$response,'menu6/notice.twig');
+        return $this->render($this->twig,$response,'menu6/notice.twig',[
+            'lists' => $this->boardService->list($request, BoardType::Notice),
+        ]);
     }
 
     public function noticeDetail(Request $request, Response $response): Response
